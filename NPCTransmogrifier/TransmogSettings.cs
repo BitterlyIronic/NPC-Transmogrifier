b6c0941364a50d9472a82735bb7b600c4cc0980b
Piranha91 <piranha9191@gmail.com>
2023-12-10 16:02:37 -0800

    Initial Code

---
+++ NPCTransmogrifier/TransmogSettings.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPCTransmogrifier
+{
+    internal class TransmogSettings
+    {
+        public int BrowsForwardVsBack_Min { get; set; } = 0;
+        public int BrowsForwardVsBack_Max { get; set; } = 1;
+        public int BrowsInVsOut_Min { get; set; } = 0;
+        public int BrowsInVsOut_Max { get; set; } = 1;
+        public int BrowsUpVsDown_Min { get; set; } = 0;
+        public int BrowsUpVsDown_Max { get; set; } = 1;
+        public int CheeksForwardVsBack_Min { get; set; } = 0;
+        public int CheeksForwardVsBack_Max { get; set; } = 1;
+        public int CheeksUpVsDown_Min { get; set; } = 0;
+        public int CheeksUpVsDown_Max { get; set; } = 1;
+        public int ChinNarrowVsWide_Min { get; set; } = 0;
+        public int ChinNarrowVsWide_Max { get; set; } = 1;
+        public int ChinUnderbiteVsOverbite_Min { get; set; } = 0;
+        public int ChinUnderbiteVsOverbite_Max { get; set; } = 1;
+        public int ChinUpVsDown_Min { get; set; } = 0;
+        public int ChinUpVsDown_Max { get; set; } = 1;
+        public int EyesForwardVsBack_Min { get; set; } = 0;
+        public int EyesForwardVsBack_Max { get; set; } = 1;
+        public int EyesInVsOut_Min { get; set; } = 0;
+        public int EyesInVsOut_Max { get; set; } = 1;
+        public int EyesUpVsDown_Min { get; set; } = 0;
+        public int EyesUpVsDown_Max { get; set; } = 1;
+        public int JawForwardVsBack_Min { get; set; } = 0;
+        public int JawForwardVsBack_Max { get; set; } = 1;
+        public int JawNarrowVsWide_Min { get; set; } = 0;
+        public int JawNarrowVsWide_Max { get; set; } = 1;
+        public int JawUpVsDown_Min { get; set; } = 0;
+        public int JawUpVsDown_Max { get; set; } = 1;
+        public int LipsInVsOut_Min { get; set; } = 0;
+        public int LipsInVsOut_Max { get; set; } = 1;
+        public int LipsUpVsDown_Min { get; set; } = 0;
+        public int LipsUpVsDown_Max { get; set; } = 1;
+        public int NoseLongVsShort_Min { get; set; } = 0;
+        public int NoseLongVsShort_Max { get; set; } = 1;
+        public int NoseUpVsDown_Min { get; set; } = 0;
+        public int NoseUpVsDown_Max { get; set; } = 1;
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# NPC-Transmogrifier
+# NPC Transmogrifier
+
+This patcher is supposed to randomize NPC facemorphs. After running it, you need to either regenerate FaceGen in the CK, or have Face Discoloration Fix installed. This will interfere with custom face sculpt NPCs.
+
+This patcher is created by request and immediately placed for adoption on the Synthesis Collective. I will not be providing support to users.
